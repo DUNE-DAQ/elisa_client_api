9a08b34b76d62c78224d57ba5ab67a52876e2233
Pierre Lasorak <pierre.lasorak@gmail.com>
2021-11-15 14:05:07 +0100

    pip complient

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
+setup(
+    name="elisa_client_api",
+    install_requires=[
+        "requests",
+        # "urllib",
+        # "mimetypes",
+        # "xml",
+        # "http"
+    ]
+)
